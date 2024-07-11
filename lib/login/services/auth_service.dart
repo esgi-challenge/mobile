d63db6cff5623cb6e27f1ee573d71da03ce82cfd
Antoine <antoine.lorin2@gmail.com>
2024-07-11 11:23:35 +0200

    wip: feat: add information page (#5)

--- lib/login/services/auth_service.dart
+++ lib/login/services/auth_service.dart
@@ -1,19 +1,24 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
-  static const _authKey = 'kAuth';
+  static const authKey = 'kAuth';
 
   static String? jwt;
 
   static Future<String?> init() async {
     final prefs = await SharedPreferences.getInstance();
-    jwt = prefs.getString(_authKey);
+    jwt = prefs.getString(authKey);
     return jwt;
   }
 
   static Future<String?> saveJwt(String jwt) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_authKey, jwt);
+    await prefs.setString(authKey, jwt);
     return jwt;
+  }
+
+  static Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(authKey);
   }
 }
